06aa86608bf29d85d48fcef231342207a8798652
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-21 20:42:21 +0530

    feat: implemented reverse string  program

--- practice-programs/ReverseString.java
+++ practice-programs/ReverseString.java
@@ -4,7 +4,30 @@
  */
 public class ReverseString {
 
+    private static String reverseString(String str)  {
+        if(str == null || str.isEmpty()) {
+            return str;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--){
+            sb.append(str.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        // Your code here
+       String str = "Hello Amrit";
+
+       String reverse = reverseString(str);
+       System.out.println("Reversed string using loop: " + reverse);
+
+
+       // using StringBuilder
+       StringBuilder sb = new StringBuilder(str);
+       sb.reverse();
+       System.out.println("Reversed string: " + sb.toString());
+
     }
 }
