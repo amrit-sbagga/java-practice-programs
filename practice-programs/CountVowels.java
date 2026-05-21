f50e5282d88dfeecd092a6951cf16349c8aaea99
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-21 20:21:47 +0530

    feat: implemented countvowels program

--- practice-programs/CountVowels.java
+++ practice-programs/CountVowels.java
@@ -1,10 +1,33 @@
+import java.util.Arrays;
+
 /**
  * Requirement:
  * Count vowels (a,e,i,o,u) in a string — case insensitive.
  */
 public class CountVowels {
 
+    private static int countVowels(String str) {
+        if(str == null || str.isEmpty()){
+          return 0;
+        }
+
+        String vowels = "aeiou";
+        int count = 0;
+
+        for(char ch : str.toCharArray()) {
+            char lower = Character.toLowerCase(ch);
+            if(vowels.indexOf(lower) >= 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        // Your code here
+
+        String str = "Hello Amrit";
+        int count = countVowels(str);
+        System.out.println("Count of vowels: " + count);
     }
 }
