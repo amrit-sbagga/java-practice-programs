6cc735ba8575b507dcdb3694800025faa4855f6d
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-21 19:20:39 +0530

    feat: Implemented anagram check program

--- practice-programs/AnagramCheck.java
+++ practice-programs/AnagramCheck.java
@@ -1,10 +1,32 @@
+import java.util.Arrays;
+
 /**
  * Requirement:
  * Check if two strings are anagrams (same characters, different order).
  */
 public class AnagramCheck {
 
+    private static boolean isAnagram(String str1, String str2) {
+        if(str1.length() != str2.length()){
+            return false;
+        }
+
+        char[] str1Chars = str1.toCharArray();
+        char[] str2Chars = str2.toCharArray();
+
+        Arrays.sort(str1Chars);
+        Arrays.sort(str2Chars);
+
+        return Arrays.equals(str1Chars, str2Chars);
+    }
+
     public static void main(String[] args) {
         // Your code here
+
+        String str1 = "listen";
+        String str2 = "silent";
+
+        boolean isAnagram = isAnagram(str1, str2);
+        System.out.println("isAnagram: " + isAnagram);
     }
 }
