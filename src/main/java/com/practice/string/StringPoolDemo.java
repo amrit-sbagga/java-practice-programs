7aea22cf5f4515cdafa74814fe317222b7680a9a
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-23 02:29:59 +0530

    chore: practice string poll demo

--- src/main/java/com/practice/string/StringPoolDemo.java
+++ src/main/java/com/practice/string/StringPoolDemo.java
@@ -11,9 +11,9 @@ public class StringPoolDemo {
         String c = new String("hello");
         String d = c.intern();
 
-        System.out.println("a == b: " + (a == b));
-        System.out.println("a == c: " + (a == c));
-        System.out.println("a == d: " + (a == d));
-        System.out.println("a.equals(c): " + a.equals(c));
+        System.out.println("a == b: " + (a == b)); // true because both refer to the same object in the pool
+        System.out.println("a == c: " + (a == c)); // false because c is a new object
+        System.out.println("a == d: " + (a == d)); // false because d is a new object
+        System.out.println("a.equals(c): " + a.equals(c)); // true because both refer to the different objects
     }
 }
