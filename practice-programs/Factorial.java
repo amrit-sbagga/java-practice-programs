da69c49f027aa0e23afae00589c096c49acb9997
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-23 02:30:37 +0530

    feat: implemented factorial program

--- practice-programs/Factorial.java
+++ practice-programs/Factorial.java
@@ -1,10 +1,26 @@
+import java.math.BigInteger;
+
 /**
  * Requirement:
- * Compute factorial of n (n!). Handle n = 0. Use iterative or recursive approach.
+ * Compute factorial of n (n!). Handle n = 0. Use iterative or recursive
+ * approach.
  */
 public class Factorial {
 
+    private static BigInteger findFactorial(BigInteger n) {
+        if (n.signum() < 0) {
+            throw new IllegalArgumentException("n must be >= 0, got: " + n);
+        }
+        if (n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE)) {
+            return BigInteger.ONE;
+        }
+        return n.multiply(findFactorial(n.subtract(BigInteger.ONE)));
+    }
+
     public static void main(String[] args) {
-        // Your code here
+
+        int n = 25;
+        BigInteger factorial = findFactorial(BigInteger.valueOf(n));
+        System.out.println("Factorial of " + n + " is " + factorial);
     }
 }
