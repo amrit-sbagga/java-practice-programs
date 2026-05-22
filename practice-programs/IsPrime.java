b0a175bd490aa06b0688f73b065c034ae032edc8
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-23 02:30:55 +0530

    feat: implemented isprime program

--- practice-programs/IsPrime.java
+++ practice-programs/IsPrime.java
@@ -4,7 +4,25 @@
  */
 public class IsPrime {
 
+    private static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        // Your code here
+        int num1 = 18;
+        boolean isPrime = isPrime(num1);
+        System.out.println("Is " + num1 + " prime? " + isPrime);
+    
+        int num2 = 19;
+        boolean isPrime2 = isPrime(num2);
+        System.out.println("Is " + num2 + " prime? " + isPrime2);
     }
 }
