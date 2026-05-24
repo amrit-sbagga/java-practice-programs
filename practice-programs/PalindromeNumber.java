9f18f0ad264113714d102cf344b8385b49614e73
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-24 21:25:41 +0530

    feat: implemented palindrome number

--- practice-programs/PalindromeNumber.java
+++ practice-programs/PalindromeNumber.java
@@ -4,7 +4,28 @@
  */
 public class PalindromeNumber {
 
+    private static boolean isPalindromeNumber(int num) {
+        int originalNum = num;
+
+        int reversedNum = 0;
+
+        while(num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return reversedNum == originalNum;
+    }
+
     public static void main(String[] args) {
-        // Your code here
+        int[] nums = new int[] {121, 1331, 12321, 12345};
+        for(int num : nums) {
+            if(isPalindromeNumber(num)) {
+                System.out.println(num + " is a palindrome number");
+            } else {
+                System.out.println(num + " is not a palindrome number");
+            }
+        }
     }
 }
