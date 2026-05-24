97727cc340ec96615161e32eb27fd27f96cdd545
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-24 21:25:21 +0530

    feat: implemented armstrong number

--- practice-programs/ArmstrongNumber.java
+++ practice-programs/ArmstrongNumber.java
@@ -4,7 +4,34 @@
  */
 public class ArmstrongNumber {
 
+    private static boolean isArmstrongNumber(int num) {
+        int originalNum = num;
+
+        int sum = 0;
+
+        int digitCount = String.valueOf(num).length();
+
+        while(num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digitCount);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+
+    }
+
     public static void main(String[] args) {
-        // Your code here
+        
+        int[] nums = new int[] {153, 172, 370, 371, 407};
+        
+        for(int num : nums) {
+            if(isArmstrongNumber(num)) {
+                System.out.println(num + " is an Armstrong number");
+            } else {
+                System.out.println(num + " is not an Armstrong number");
+            }
+        }
+
     }
 }
