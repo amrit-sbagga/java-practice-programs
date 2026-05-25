9ff0e40c8e4d4a0cf93b730b21762dfe2c2ed8e8
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-26 01:18:55 +0530

    feat: implemented bubble sort program

--- practice-programs/BubbleSort.java
+++ practice-programs/BubbleSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Requirement:
  * Sort an int array using bubble sort. Print sorted array.
@@ -5,6 +7,21 @@
 public class BubbleSort {
 
     public static void main(String[] args) {
-        // Your code here
+        int[] nums = { 64, 34, 25, 12, 22, 11, 90 };
+        bubbleSort(nums);
+        System.out.println("Sorted array: " + Arrays.toString(nums));
+    }
+
+    private static void bubbleSort(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 }
